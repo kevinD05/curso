cbb7b21c427239d39ec3a5f631f4b8c66636da2a
Kevin Diaz <alexanderkevindiaz05@gmial.com>
2022-07-01 14:12:23 -0600

    tipo de datos

--- curso.py
+++ curso.py
@@ -1,3 +1,23 @@
 #esto es un comentario
 print('hola')
 "esto es otro comentario"
+
+#tipo de datos entero o largo
+numero = 15
+print(numero, type (numero))
+
+#tipo de datos flotante
+numero_flotante = 15.5
+print(numero, type (numero_flotante))
+
+#tipo de numero complejo
+numero_complejo = 5 + 6j
+print(numero_complejo, type(numero_complejo))
+
+#tipo de datos string
+nombre = 'kevin'
+print(nombre, type(nombre))
+
+#tipo de dato booleano
+verdadero_falso = 4==4
+print(verdadero_falso, type(verdadero_falso))
