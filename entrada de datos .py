b8b4183540db5bf19c53a19be400af25e6975958
Kevin Diaz <alexanderkevindiaz05@gmial.com>
2022-07-01 14:34:14 -0600

    entrada de datos

---
+++ entrada de datos .py
@@ -0,0 +1,9 @@
+palabra = input('introduce una palabra:')
+num_int = int(input('introduce un numero entero:'))
+num_float = float(input('introduce un numero flotante:'))
+num_compless = complex(input('introduce un numero complejo'))
+
+print('string', palabra)
+print('numero entero', num_int)
+print('numero flotante',num_float)
+print('numero complejo',num_compless)
